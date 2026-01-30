d9fe3876988d82924ba39a342940b4c038b16533
krevindev <alimpuangonkylerevin@gmail.com>
2026-01-31 02:36:08 +0800

    fix: fixed the width

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Hero from "../sections/Hero";
 
 export default function Home() {
   return (
-    <div className="w-screen h-fit">
+    <div className="w-screen h-fit px-20 pt-30">
       <Hero />
       <Hero />
       <Hero />
