3910424492c5bcb5b0f114878138edc2d8123eab
krevindev <alimpuangonkylerevin@gmail.com>
2026-01-31 02:35:03 +0800

    fix: changed the position to fixed and added backdrop blur effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,23 @@
-import images from '../constants/images'
+import images from "../constants/images";
 
 export default function Header() {
   return (
-    <div className="bg-blue-400 w-full fixed flex justify-between items-center box-border px-10">
-        <div>
-            <img className='h-20' src={images.mainLogo}/>
-        </div>
-        <div>
-            <ul className='flex'>
-                <li>Home</li>
-                <li>Services</li>
-                <li>About Us</li>
-            </ul>
-        </div>
+    <div className=" w-full fixed z-[100] px-20 top-0 left-0 flex justify-between items-center box-border z-10 text-white h-30">
+      {/* Right Container (Logo) */}
+      <div>
+        <img className="h-20 backdrop-blur-sm px-5 rounded-xl" src={images.mainLogo} />
+      </div>
+      {/* Left Container (Menu Nav)*/}
+      <div className="w-1/2 flex justify-end">
+        <ul className="px-5 rounded-2xl flex justify-end h-full items-stretch backdrop-blur-sm py-5">
+          <li className="mr-10">Home</li>
+          <li className="mr-10">Services</li>
+          <li className="mr-10">About Us</li>
+          <button className="bg-white text-blue-300 px-5 rounded-lg">
+            Contact
+          </button>
+        </ul>
+      </div>
     </div>
   );
 }
