cec1407e00f498d0bf457383f9087382c3e3ed27
krevindev <alimpuangonkylerevin@gmail.com>
2026-01-31 19:44:28 +0800

    chore: added the services icon references

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,24 @@
 import mainLogo from "../assets/logos/main-logo.png";
-import heroImage from "../assets/images/hero-image.png";
+import heroImage from "../assets/images/hero-image.jpg";
+
+// Services
+import service1Icon from "../assets/icons/service-1.svg";
+import service2Icon from "../assets/icons/service-2.svg";
+import service3Icon from "../assets/icons/service-3.svg";
+import service4Icon from "../assets/icons/service-4.svg";
+import service5Icon from "../assets/icons/service-5.svg";
+import service6Icon from "../assets/icons/service-6.svg";
+
+import heroPhoneIcon from "../assets/icons/hero-phone.svg";
+
 export default {
   mainLogo,
   heroImage,
+  service1Icon,
+  service2Icon,
+  service3Icon,
+  service4Icon,
+  service5Icon,
+  service6Icon,
+  heroPhoneIcon
 };
