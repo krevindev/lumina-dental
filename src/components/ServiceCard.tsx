1e55c01d8cc2f85c67d39837c6d952ee0bbe4a96
krevindev <alimpuangonkylerevin@gmail.com>
2026-01-31 21:24:02 +0800

    fix: adjusted the layout sizes to compress the element

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -6,12 +6,12 @@ type ServiceCardProps = {
 
 const ServiceCard = ({ title, description, imgSrc }: ServiceCardProps) => {
   return (
-    <div className="bg-white rounded-3xl h-full p-10 py-8 shadow-lg hover:-translate-y-1 transition-all ease-in">
-      <div className="flex justify-start items-center mb-2">
-        <img src={imgSrc} className="mr-5" />
-        <h1 className="text-3xl text-left font-bold text-[#6591DE]">{title}</h1>
+    <div className="bg-white rounded-3xl h-full py-6 px-10 shadow-lg hover:-translate-y-1 transition-all ease-in grid grid-rows-2">
+      <div className="flex justify-start items-center">
+        <img src={imgSrc} className="mr-3 size-13" />
+        <h1 className="text-2xl text-left font-bold text-[#6591DE]">{title}</h1>
       </div>
-      <p className="text-lg mt-3 text-[#6591DE] text-justify">{description}</p>
+      <p className="text-lg text-[#6591DE] text-justify">{description}</p>
     </div>
   );
 };
