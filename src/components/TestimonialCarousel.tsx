0095d795792ff894e941125a2cdefdf0a5f31fe2
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-01 19:10:00 +0800

    chore: initialized custom testimonials carousel from shacdn

---
+++ src/components/TestimonialCarousel.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import * as React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+
+export default function TestimonialCarousel() {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  return (
+    <div className="mx-auto max-w-[10rem] sm:max-w-xs">
+      <Carousel setApi={setApi} className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Card className="m-px rounded-br-[50px]">
+                <CardContent className="flex flex-col items-center justify-center p-6 text-[#6591DE]">
+                  <p className="text-[#6591DE]">
+                    “Lumina Dental made my visit easy and comfortable, with
+                    staff who explained everything clearly.”
+                  </p>
+                  <h4 className="font-bold mt-5 text-xl">Mark Anthony Reyes</h4>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="text-muted-foreground py-2 text-center text-sm flex justify-center items-center">
+        {/* Slide {current} of {count} */}
+        {Array.from({ length: count }).map((_, idx) => (
+          <div
+            className={`border-white border rounded-full size-2 m-1 mt-5 ${idx+1 === current ? "bg-white" : "bg-transparent"}`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
