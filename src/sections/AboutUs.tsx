2792026f265898260d8f86a119c3f416c2accd3c
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-01 16:04:24 +0800

    chore: added the meet the experts subsection

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -1,10 +1,11 @@
+import MeetTheExperts from "./about-us-sections/MeetTheExperts";
 import TrustedCare from "./about-us-sections/TrustedCare";
 
 export default function AboutUs() {
   return (
     <div className="w-full min-h-screen py-10">
-      {/* Trusted Care */}
       <TrustedCare />
+      <MeetTheExperts/>
     </div>
   );
 }
