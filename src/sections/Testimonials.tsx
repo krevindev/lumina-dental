153ab08f1080e4b58ef168b4252b7490cc3a1667
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-01 20:18:58 +0800

    chore: added the circle and glow bg elements

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -2,8 +2,12 @@ import TestimonialCarousel from "@/components/TestimonialCarousel";
 
 export default function Testimonials() {
   return (
-    <div className="max-h-screen min-h-[70vh] h-[70vh] w-full text-white grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))]">
-      <div className=" flex justify-center items-start flex-col text-left">
+    <div className="max-h-screen min-h-[70vh] h-[70vh] w-full text-white grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] z-10 relative">
+      <div className="bg-[#739FEC] w-125 h-125 opacity-60 absolute rounded-full z-0 left-[15%] -top-[90%] translate-y-[20%]" />
+      <div className="bg-white w-125 h-125 fixed left-0 bottom-0 -translate-x-[80%] translate-y-[50%] rounded-full blur-[150px] z-0 opacity-40"></div>
+
+      <div className=" flex justify-center items-start flex-col text-left z-10">
+
         <h1 className="text-5xl">
           Complete <b>Dental Care</b>, <br></br>
           <b>Personalized</b> for You
