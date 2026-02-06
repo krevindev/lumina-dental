c3752277988d740217eceaaf0aceb0a2511ea24a
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-07 07:32:37 +0800

    fix: changed the embedded maps to zoomed out versions

--- src/sections/about-us-sections/OurLocation.tsx
+++ src/sections/about-us-sections/OurLocation.tsx
@@ -25,7 +25,10 @@ const BranchName = ({
         <div className="absolute w-full h-0.5 bg-[#8FB9FF] left-full -translate-y-1/2 top-1/2"></div>
       )} */}
       <div className="row-span-1 col-span-1 w-fit h-fit row-start-1 col-start-1 flex justify-start items-center">
-        <img className="w-[30px] min-w-fit pr-5 sm:pr-5" src={images.locationPin} />
+        <img
+          className="w-[30px] min-w-fit pr-5 sm:pr-5"
+          src={images.locationPin}
+        />
       </div>
       <h2 className="text-sm font-medium col-start-2 row-start-1 col-span-15 flex items-center h-fit">
         {branchName}
@@ -47,9 +50,10 @@ const branchesData = [
     location: "San Dico St, Diamond Subd, Angeles, 2009 Pampanga",
   },
 ];
+
 const locationSrcs = [
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3850.8735024575362!2d120.59272899999998!3d15.1652902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f2744b40e50f%3A0xe518d7ba71ad65a8!2sMarcelius%20St%2C%20Balibago%2C%20Angeles%2C%20Pampanga!5e0!3m2!1sen!2sph!4v1769935924848!5m2!1sen!2sph",
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15403.87425760549!2d120.59743780000001!3d15.160070999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f3867ae0be61%3A0xab5211f9f351412c!2sDiamond%20subd%2C%2010-1%20Road%201%2C%20Balibago%2C%20Angeles%2C%202009%20Pampanga!5e0!3m2!1sen!2sph!4v1769938765334!5m2!1sen!2sph",
+  "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7781.973543578954!2d120.592729!3d15.16529!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f2744b40e50f%3A0xe518d7ba71ad65a8!2sMarcelius%20St%2C%20Balibago%2C%20Angeles%2C%20Pampanga!5e0!3m2!1sen!2sph!4v1770420659086!5m2!1sen!2sph",
+  "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d62257.325124488845!2d120.597438!3d15.160070999999999!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f3867ae0be61%3A0xab5211f9f351412c!2sDiamond%20subd%2C%2010-1%20Road%201%2C%20Balibago%2C%20Angeles%2C%202009%20Pampanga!5e0!3m2!1sen!2sph!4v1770420490931!5m2!1sen!2sph",
 ];
 
 export default function OurLocation() {
