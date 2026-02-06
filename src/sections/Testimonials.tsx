3dc8709332a2df76b76a79f9df87a422eb2c0bd2
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-07 07:11:45 +0800

    fix: centered the text on mobile

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,10 +7,10 @@ export default function Testimonials() {
       <div className="bg-white w-125 h-125 fixed left-0 bottom-0 -translate-x-[80%] translate-y-[50%] rounded-full blur-[150px] z-0 opacity-40"></div>
 
       <div className="pb-10 md:pb-0 flex justify-center items-start flex-col text-left z-10">
-        <h1 className="text-[clamp(24px,9vw,50px)] text-left w-full lg:text-[clamp(15px,9vw,50px)] leading-tight lg:leading-15">
+        <h1 className="text-[clamp(24px,9vw,50px)]  w-full lg:text-[clamp(15px,9vw,50px)] leading-tight lg:leading-15 text-center md:text-left">
           Complete <b>Dental Care</b>, <b>Personalized</b> for You
         </h1>
-        <p className="text-[clamp(7px,4vw,20px)] w-full text-left font-light mt-2 flex justify-start items-start">
+        <p className="text-[clamp(7px,4vw,20px)] w-full font-light mt-2 flex justify-start items-start text-center md:text-left">
           Patient feedback reflects the quality of care, professionalism, and
           patient focused approach provided by our clinic.
         </p>
