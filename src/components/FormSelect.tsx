38963e53d2ea559ec1b0a20120512bd755b9e55a
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-08 06:04:13 +0800

    chore: changed the form seletion placeholder

--- src/components/FormSelect.tsx
+++ src/components/FormSelect.tsx
@@ -14,20 +14,26 @@ export function FormSelect() {
       <SelectTrigger className="border my-2 border-[rgba(101,145,223,.43)] text-[rgba(101,145,223,.78)]  focus:border-[rgba(101,145,223,.43)] rounded-lg font-light">
         <SelectValue
           className="text-[#6591DF] placeholder:text-[rgba(101,145,223,.78)] placeholder:text-sm placeholder:font-light "
-          placeholder="Select service"
+          placeholder="Purpose of Inquiry"
         />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
           <SelectLabel>Purpose of Inquiry</SelectLabel>
           <SelectItem value="general-inquiry">General Inquiry</SelectItem>
-          <SelectItem value="appointment-request">Appointment Request</SelectItem>
+          <SelectItem value="appointment-request">
+            Appointment Request
+          </SelectItem>
           <SelectItem value="billing-questions">
             Billing or Insurance Questions
           </SelectItem>
-          <SelectItem value="treatment-consultation">Treatment Consultation</SelectItem>
+          <SelectItem value="treatment-consultation">
+            Treatment Consultation
+          </SelectItem>
           <SelectItem value="feedback">Feedback or Suggestions</SelectItem>
-          <SelectItem value="partnership">Partnership / Collaboration</SelectItem>
+          <SelectItem value="partnership">
+            Partnership / Collaboration
+          </SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
