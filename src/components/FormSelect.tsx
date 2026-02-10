52e4dbd98d4eea2fa791474a46c41e9e62b16fe9
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-11 04:09:21 +0800

    fix: changed the text colors and hover behaviors on selection

--- src/components/FormSelect.tsx
+++ src/components/FormSelect.tsx
@@ -11,27 +11,49 @@ import {
 export function FormSelect() {
   return (
     <Select>
-      <SelectTrigger className="border my-2 border-[rgba(101,145,223,.43)] text-[rgba(101,145,223,.78)]  focus:border-[rgba(101,145,223,.43)] rounded-lg font-light">
+      <SelectTrigger className="border my-2 border-[rgba(101,145,223,.43)] text-[rgba(101,145,223,1)] focus:border-[rgba(101,145,223,.43)] rounded-lg font-light">
         <SelectValue
-          className="text-[#6591DF] placeholder:text-[rgba(101,145,223,.78)] placeholder:text-sm placeholder:font-light "
+          className="text-red-400"
           placeholder="Purpose of Inquiry"
         />
       </SelectTrigger>
       <SelectContent>
-        <SelectGroup>
+        <SelectGroup className="text-[rgba(101,145,223,1)]">
           <SelectLabel>Purpose of Inquiry</SelectLabel>
-          <SelectItem value="general-inquiry">General Inquiry</SelectItem>
-          <SelectItem value="appointment-request">
+          <SelectItem
+            className="hover:bg-[#6591df] hover:text-white"
+            value="general-inquiry"
+          >
+            General Inquiry
+          </SelectItem>
+          <SelectItem
+            className="hover:bg-[#6591df] hover:text-white"
+            value="appointment-request"
+          >
             Appointment Request
           </SelectItem>
-          <SelectItem value="billing-questions">
+          <SelectItem
+            className="hover:bg-[#6591df] hover:text-white"
+            value="billing-questions"
+          >
             Billing or Insurance Questions
           </SelectItem>
-          <SelectItem value="treatment-consultation">
+          <SelectItem
+            className="hover:bg-[#6591df] hover:text-white"
+            value="treatment-consultation"
+          >
             Treatment Consultation
           </SelectItem>
-          <SelectItem value="feedback">Feedback or Suggestions</SelectItem>
-          <SelectItem value="partnership">
+          <SelectItem
+            className="hover:bg-[#6591df] hover:text-white"
+            value="feedback"
+          >
+            Feedback or Suggestions
+          </SelectItem>
+          <SelectItem
+            className="hover:bg-[#6591df] hover:text-white"
+            value="partnership"
+          >
             Partnership / Collaboration
           </SelectItem>
         </SelectGroup>
