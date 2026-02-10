7638feb2af8a7264eb906681efb7fda90db3ab13
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-11 04:21:34 +0800

    feat: initialized selection feature

--- src/components/FormSelect.tsx
+++ src/components/FormSelect.tsx
@@ -7,11 +7,27 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useState } from "react";
 
 export function FormSelect() {
+  const [selectedIndex, setSelectedIndex] = useState<number | undefined>(
+    undefined,
+  );
+
+  const inquiries: string[] = [
+    "General Inquiry",
+    "Appointment Request",
+    "Billing or Insurance Questions",
+    "Treatment Consultation",
+    "Feedback or Suggestions",
+    "Partnership / Collaboration",
+  ];
+
   return (
     <Select>
-      <SelectTrigger className="border my-2 border-[rgba(101,145,223,.43)] text-[rgba(101,145,223,1)] focus:border-[rgba(101,145,223,.43)] rounded-lg font-light">
+      <SelectTrigger
+        className={`border my-2 border-[rgba(101,145,223,.43)] focus:border-[rgba(101,145,223,.43)] rounded-lg font-light ${selectedIndex !== undefined ? "text-[rgba(101,145,223,1)]" : "text-[rgba(101,145,223,.5)]"}`}
+      >
         <SelectValue
           className="text-red-400"
           placeholder="Purpose of Inquiry"
@@ -19,7 +35,10 @@ export function FormSelect() {
       </SelectTrigger>
       <SelectContent>
         <SelectGroup className="text-[rgba(101,145,223,1)]">
-          <SelectLabel>Purpose of Inquiry</SelectLabel>
+          <SelectLabel>
+            Purpose of Inquiry:{" "}
+            {selectedIndex !== undefined && inquiries[selectedIndex]}
+          </SelectLabel>
           <SelectItem
             className="hover:bg-[#6591df] hover:text-white"
             value="general-inquiry"
