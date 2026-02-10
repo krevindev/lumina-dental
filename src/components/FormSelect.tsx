467080a94aa589b82c4110711997820c3e915f05
krevindev <alimpuangonkylerevin@gmail.com>
2026-02-11 05:21:23 +0800

    fix: fixed the font changes on selection input

--- src/components/FormSelect.tsx
+++ src/components/FormSelect.tsx
@@ -7,71 +7,64 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useState } from "react";
 
 export function FormSelect() {
-  const [selectedIndex, setSelectedIndex] = useState<number | undefined>(
-    undefined,
-  );
-
-  const inquiries: string[] = [
-    "General Inquiry",
-    "Appointment Request",
-    "Billing or Insurance Questions",
-    "Treatment Consultation",
-    "Feedback or Suggestions",
-    "Partnership / Collaboration",
-  ];
-
   return (
     <Select>
       <SelectTrigger
-        className={`border my-2 border-[rgba(101,145,223,.43)] focus:border-[rgba(101,145,223,.43)] rounded-lg font-light ${selectedIndex !== undefined ? "text-[rgba(101,145,223,1)]" : "text-[rgba(101,145,223,.5)]"}`}
+        className="
+          my-2 rounded-lg border
+          border-[rgba(101,145,223,.43)]
+          focus:border-[rgba(101,145,223,.43)]
+          text-[rgba(101,145,223,1)]
+          data-[placeholder]:opacity-50 data-[placeholder]:font-light
+        "
       >
-        <SelectValue
-          className="text-red-400"
-          placeholder="Purpose of Inquiry"
-        />
+        <SelectValue placeholder="Purpose of Inquiry" />
       </SelectTrigger>
+
       <SelectContent>
         <SelectGroup className="text-[rgba(101,145,223,1)]">
-          <SelectLabel>
-            Purpose of Inquiry:{" "}
-            {selectedIndex !== undefined && inquiries[selectedIndex]}
-          </SelectLabel>
+          <SelectLabel>Purpose of Inquiry</SelectLabel>
+
           <SelectItem
-            className="hover:bg-[#6591df] hover:text-white"
             value="general-inquiry"
+            className="hover:bg-[#6591df] hover:text-white"
           >
             General Inquiry
           </SelectItem>
+
           <SelectItem
-            className="hover:bg-[#6591df] hover:text-white"
             value="appointment-request"
+            className="hover:bg-[#6591df] hover:text-white"
           >
             Appointment Request
           </SelectItem>
+
           <SelectItem
-            className="hover:bg-[#6591df] hover:text-white"
             value="billing-questions"
+            className="hover:bg-[#6591df] hover:text-white"
           >
             Billing or Insurance Questions
           </SelectItem>
+
           <SelectItem
-            className="hover:bg-[#6591df] hover:text-white"
             value="treatment-consultation"
+            className="hover:bg-[#6591df] hover:text-white"
           >
             Treatment Consultation
           </SelectItem>
+
           <SelectItem
-            className="hover:bg-[#6591df] hover:text-white"
             value="feedback"
+            className="hover:bg-[#6591df] hover:text-white"
           >
             Feedback or Suggestions
           </SelectItem>
+
           <SelectItem
-            className="hover:bg-[#6591df] hover:text-white"
             value="partnership"
+            className="hover:bg-[#6591df] hover:text-white"
           >
             Partnership / Collaboration
           </SelectItem>
